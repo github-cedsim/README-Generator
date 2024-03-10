230b8e6aba34d8b1a60abaa6ceffe1c95a401bbf
Cedric Simpkins <126218396+github-cedsim@users.noreply.github.com>
2024-03-10 16:00:07 -0500

    Added functions and additional javascript to both js files

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,50 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./Develop/utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+function createQuestion(name, message) {
+    return {
+      type: 'input',
+      name: name,
+      message: message,
+    };
+  }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const questions = [
+  createQuestion('title', 'What is the title of your project?'),
+  createQuestion('description', 'Provide a description of the project:'),
+  createQuestion('installation', 'How is your project installed?'),
+  createQuestion('usage', 'How is your project used?'),
+  createQuestion('contribution', 'How can others contribute to your project?'),
+  createQuestion('tests', 'How can your project be tested?'),
+  // For 'license', since it's a 'list', it doesn't fit the createQuestion mold and should be added manually
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+  },
+  createQuestion('github', 'Enter your GitHub Username:'),
+  createQuestion('email', 'Enter your email address:'),
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Successfully created README.md!')
+  );
+}
+
+// Function to initialize the app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdownContent = generateMarkdown(answers);
+        writeToFile('README.md', markdownContent);
+      });
+} 
+
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,70 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license || license === 'None') return '';
+  const badgeLinks = {
+    'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+    'Apache 2.0': 'https://img.shields.io/badge/License-Apache_2.0-blue.svg',
+    'GPL 3.0': 'https://img.shields.io/badge/License-GPLv3-blue.svg',
+    'BSD 3-Clause': 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg',
+  };
+  return `![License Badge](${badgeLinks[license]})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license || license === 'None') return '';
+  const licenseLinks = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'GPL 3.0': 'https://www.gnu.org/licenses/gpl-3.0',
+    'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause',
+  };
+  return `[License Link](${licenseLinks[license]})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === 'None') return '';
+  return `## License
+This project is licensed under the ${license} license. ${renderLicenseLink(license)}`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ${data.license && data.license !== 'None' ? '- [License](#license)' : ''}
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Contributing
+  ${data.contribution}
+  
+  ## Tests
+  ${data.tests}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Questions
+  For any questions, please reach out to me at [${data.email}](mailto:${data.email}).
+  GitHub: [${data.github}](https://github.com/${data.github})
+  `;
 }
 
 module.exports = generateMarkdown;
